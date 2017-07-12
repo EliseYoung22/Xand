e91235338142cfe62e20bc096a20f3e162b14db9
Elise Young <eliseyoung_4@hotmail.com>
2017-07-12 22:28:14 +1000

    add dev contact form

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'eliseyoung_4@hotmail.com'
   layout 'mailer'
 end
---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,13 @@
+class ContactMailer < ApplicationMailer
+  default from: 'enquiries@xandinnovations.com'
+
+  def send_contact_email(email, message)
+    @email = email
+    @message = message
+    mail(to: 'eliseyoung_4@hotmail.com', subject: 'New message from website') do |format|
+      format.html { render 'send_contact_email' }
+      format.text { render 'send_contact_email' }
+    end
+  end
+
+end
