d3a8258e8ce4b3a53fa02ddecd25a7ecd3cce1aa
Elise Young <eliseyoung_4@hotmail.com>
2017-07-23 19:11:14 +1000

    configure image upload for cloudinary

--- app/models/dental_product.rb
+++ app/models/dental_product.rb
@@ -1,4 +1,4 @@
 class DentalProduct < ApplicationRecord
-  has_attached_file :avatar, styles: {large:"600x600>", medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :avatar, styles: {large:"600x600>", medium: "300x300>", thumb: "100x100>" }, :default_url => "/infinity.png", :storage => :cloudinary, :path => ":id/:style/:filename"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
